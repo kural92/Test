f7391e26f342b7c1f3b4a0548531512c968da8bc
Arunkumar_Manickavasagam <arunkumar.m@netmeds.com>
2021-08-04 17:22:14 +0530

    Code changes done in Your account

--- src/test/java/com/Nm/website_stage/Youraccount.java
+++ src/test/java/com/Nm/website_stage/Youraccount.java
@@ -75,7 +75,7 @@ public class Youraccount extends BaseClass {
 //Menu : Delivery Address//
 	
 	
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void DeliverAddressSection() throws Throwable {
 
 		logger = report.createTest("DeliverAddressSection");
@@ -194,7 +194,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************MY
 	// Wallet*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Mywallet() throws Throwable {
 
 		logger = report.createTest("Mywallet");
@@ -311,7 +311,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************Edit
 	// Profile*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Editprofile() throws Throwable {
 
 		logger = report.createTest("Editprofile");
@@ -442,7 +442,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************Contact
 	// Us*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void contactus() throws Throwable {
 
 		logger = report.createTest("contact");
@@ -550,7 +550,7 @@ public class Youraccount extends BaseClass {
 	}
 
 //********************************************* My prescription***************************************************************************************
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Myprescription() throws Throwable {
 
 		logger = report.createTest("Myprescription");
@@ -774,7 +774,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************Help
 	// Page*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Helppage() throws Throwable {
 
 		logger = report.createTest("Helppage");
@@ -878,7 +878,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************Rate
 	// Us*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void rateus() throws Throwable {
 
 		logger = report.createTest("Rateus");
@@ -986,7 +986,7 @@ public class Youraccount extends BaseClass {
 	// **************************************************************Legal
 	// Information*************************************************************************************//
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Legalinformation() throws Throwable {
 
 		logger = report.createTest("Legalinformation");
