3eb3dcf0fdbf880592bdf7d4526991800b246a63
Arunkumar_Manickavasagam <arunkumar.m@netmeds.com>
2021-08-16 11:58:41 +0530

    Android- Pom code and M3 flow code

--- src/main/java/com/Nm/Pom/AndriodPom.java
+++ src/main/java/com/Nm/Pom/AndriodPom.java
@@ -13,10 +13,55 @@ public class AndriodPom extends MobileBaseClass{
 		PageFactory.initElements(driver, this);
 		
 	}
-	@FindBy(xpath="//android.widget.EditText[@resource-id='com.NetmedsMarketplace.Netmeds:id/name']")
-	private WebElement mobilenumber;
-	public WebElement getMobilenumber() {
-		return mobilenumber;
-	}
 	
+	
+	//********************************************M3 FLow
+		@FindBy(xpath = "//android.widget.TextView[@resource-id ='com.NetmedsMarketplace.Netmeds:id/lets_start']")
+		private WebElement Letsstarted;
+		public WebElement getLetsstarted() {
+			return Letsstarted;
+		}
+
+		@FindBy(xpath="//android.widget.EditText[@resource-id='com.NetmedsMarketplace.Netmeds:id/name']")
+		private WebElement mobilenumber;
+		public WebElement getMobilenumber() {
+			return mobilenumber;
+		}
+		
+		@FindBy(xpath = "//android.widget.TextView[@resource-id ='com.NetmedsMarketplace.Netmeds:id/btn_use_password']")
+		private WebElement usepwd;
+		public WebElement getUsepwd() {
+			return usepwd;
+		}
+		
+		@FindBy(xpath ="//android.widget.EditText[@resource-id='com.NetmedsMarketplace.Netmeds:id/edt_password']")
+		private WebElement password;
+		public WebElement getPassword() {
+			return password;
+		}
+		
+		@FindBy(xpath = "//android.widget.TextView[@resource-id='com.NetmedsMarketplace.Netmeds:id/txtLogin']")
+		private WebElement Login;
+		public WebElement getLogin() {
+			return Login;
+		}
+		
+		@FindBy(xpath ="//android.widget.ImageView[@resource-id='com.NetmedsMarketplace.Netmeds:id/subscription_image']")
+		private WebElement Subscription;
+		public WebElement getSubscription() {
+			return Subscription;
+		}
+		
+		@FindBy(xpath = "//android.widget.TextView[@resource-id='com.NetmedsMarketplace.Netmeds:id/tv_createSubscription']")
+		private WebElement createnew_subscription;
+		public WebElement getCreatenew_subscription() {
+			return createnew_subscription;
+		}
+		
+		//@FindBy(xpath = "//android.widget.EditText[@resourceid='com.NetmedsMarketplace.Netmeds:id/search_bar']")
+		@FindBy(id ="com.NetmedsMarketplace.Netmeds:id/search_bar")
+		private WebElement M3productsearch;
+		public WebElement getM3productsearch() {
+			return M3productsearch;
+		}
 }
---
+++ src/test/java/com/Nm/Andriod_prod/M3_flow.java
@@ -0,0 +1,269 @@
+package com.Nm.Andriod_prod;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.Nm.Base.BaseClass;
+import com.Nm.Base.MobileBaseClass;
+import com.Nm.Pom.AndriodPom;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class M3_flow extends MobileBaseClass {
+
+	
+	
+	
+	ExtentHtmlReporter htmlReporter;
+	 public static ExtentReports report;
+		public static  ExtentTest logger;
+		@BeforeClass
+		public void launchbrowser() {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability(MobileCapabilityType.UDID, "07c55fe10406");//"c195de14"
+			capabilities.setCapability("platformName", "Android");
+			//capabilities.setCapability("deviceName", "vivo 1819");
+			//capabilities.setCapability("platformVersion","10.0.0" );
+			//for m-site
+			//capabilities.setCapability("chromedriverExecutable", "D:\\Eclipse\\nm_website\\driver\\chromedriver.exe");
+			//for install Apk file
+			//capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Admin\\Downloads\\wyth_SIT_s9.10.apk");
+			// already installed app
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.NetmedsMarketplace.Netmeds");
+			capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.netmedsmarketplace.netmeds.AppUriSchemeHandler");
+			capabilities.setCapability("noReset", true);
+			
+					
+			capabilities.setCapability("autoDismissAlerts", true);  
+			
+			try {
+				driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+				
+			} catch (MalformedURLException e) {
+				System.out.println(e.getMessage());
+			}
+					
+		}
+	@BeforeTest(groups = {"forgetPassword","sanity","reg"})
+   public void startReport() {
+   	
+      htmlReporter = new ExtentHtmlReporter(".//Report//AlternateBrand.html");
+       
+       //initialize ExtentReports and attach the HtmlReporter
+       report = new ExtentReports();
+       
+     // htmlReporter.setAppendExisting(true);
+       report.attachReporter(htmlReporter);
+       report.setSystemInfo("Host name", "localhost");
+       report.setSystemInfo("Environemnt", "QA");
+       report.setSystemInfo("user", "Automation Team");
+   
+       //configuration items to change the look and feel
+       //add content, manage tests etc
+
+       htmlReporter.config().setDocumentTitle("Extent Report ");
+       htmlReporter.config().setReportName("NetMeds.com");
+
+       //htmlReporter.config().setTheme(Theme.STANDARD);
+   	
+   }
+	@Test
+	public void M3Subscriptionflow() throws Throwable {
+		
+		logger =  report.createTest("M3 - Subscription Flow");
+		logger.log(Status.PASS, "*************M3 - Subscription Flow********************" );
+		
+		AndriodPom m = new AndriodPom();
+		
+		
+		
+		
+		Thread.sleep(1000);
+		
+	try {
+		btncli(m.getLetsstarted());
+		Thread.sleep(2000);
+		m.getMobilenumber().sendKeys("8072281468");
+		Thread.sleep(3000);
+		btncli(m.getUsepwd());
+		Thread.sleep(5000);
+	
+		m.getPassword().sendKeys("test@123");
+		
+		Thread.sleep(2000);
+	
+		try {
+			WebElement close_add = 	driver.findElement(By.xpath("android.widget.TextView[@package= 'com.NetmedsMarketplace.Netmeds']"));
+			actionClick(close_add);
+			
+			//driver.findElement(By.xpath("android.widget.TextView[@index= '0']")).click();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		Thread.sleep(5000);
+		btncli(m.getLogin());
+	}
+	
+	catch(Exception e) {
+		System.out.println("Already logged in");
+	}
+	
+	
+		Thread.sleep(5000);
+		btncli(m.getSubscription());
+		Thread.sleep(6000);
+		btncli(m.getCreatenew_subscription());
+		Thread.sleep(6000);
+		btncli(m.getM3productsearch());
+		Thread.sleep(3000);
+		
+		//type(m.getM3productsearch(),"Folvite Tablet 45'S");
+		//driver.findElement(By.xpath("//android.widget.TextView[@resourceid='com.NetmedsMarketplace.Netmeds:id/add_to_cart_btn']")).click();
+		
+		
+		for (int i = 0; i < 4; i++) {
+
+			type(m.getM3productsearch(), MobileBaseClass.getExcelData("Otcandnonrx", i, 0));
+
+			// btncli(m.getSearchIcon());
+			logger.log(Status.PASS, "Successfully navigate to search result page");
+
+			//String Cart_Excel = MobileBaseClass.getExcelData("Otcandnonrx", i, 1);
+
+			// Step 7 : Add product to the cart//
+			try {
+				Thread.sleep(3000);
+
+				WebDriverWait wait = new WebDriverWait(driver, 30);
+				WebElement Cart_btn = wait.until(
+						ExpectedConditions.elementToBeClickable(By.id("com.NetmedsMarketplace.Netmeds:id/add_to_cart_btn")));
+
+				Thread.sleep(3000);
+
+				btncli(Cart_btn);
+				
+				System.out.println("successfully Items are added");
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+
+			Thread.sleep(3000);
+			
+
+			Thread.sleep(3000);
+			driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/proceedToCheckoutButton")).click();
+			driver.navigate().back();
+		}
+
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/proceedToCheckoutButton")).click();
+
+Thread.sleep(2000);
+
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/cart_proceed_btn")).click();
+Thread.sleep(2000);
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/tv_nterval_no_delivers_three")).click();
+Thread.sleep(2000);
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/tv_nterval_no_delivers_two")).click();
+Thread.sleep(2000);
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/tv_nterval_no_delivers_one")).click();
+Thread.sleep(2000);
+
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/first_checkbox")).click();
+Thread.sleep(2000);
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/second_checkbox")).click();	
+Thread.sleep(2000);
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/third_checkbox")).click();
+Thread.sleep(2000);
+
+driver.findElement(By.id("com.NetmedsMarketplace.Netmeds:id/proceed_btn")).click();
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("//android.widget.Button[@text ='MY ORDERS']")).click();
+	
+	}
+	@AfterMethod()
+	public void screenShot(ITestResult result) throws Throwable {
+		
+	    if(result.getStatus() == ITestResult.FAILURE) {
+	    	logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
+	    	logger.fail(result.getThrowable());
+	    	try {
+				
+
+				TakesScreenshot screenshot = (TakesScreenshot) driver;
+				File src = screenshot.getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(src, new File("./Report/"+ result.getName()+".png"));
+				System.out.println("Successfully captured a screenshot");
+				//logger.log(Status.FAIL, result.getThrowable());
+				logger.log(Status.FAIL, "Snapshot below: " + logger.addScreenCaptureFromPath( result.getName()+".png"));
+			
+		
+				
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+	      }
+	 
+	    else if(result.getStatus() == ITestResult.SUCCESS) {
+	    	logger.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" PASSED ", ExtentColor.GREEN));
+	    
+	    }
+	    else {
+	    
+	    	 // onFinish(context);
+	    	logger.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" SKIPPED ", ExtentColor.ORANGE));
+	    	logger.skip(result.getThrowable());
+	    	report.removeTest(logger);
+	    }
+	    
+	    
+	    
+	Thread.sleep(3000);
+	if (result.getStatus() == ITestResult.FAILURE) {
+		Thread.sleep(3000);
+	//	BaseClass.mail_report();
+	
+		
+	//	BaseClass.mail_report();
+	}
+	
+	
+	}	
+		
+
+
+	@AfterTest
+	private void quitbrowser() {
+		report.flush();
+		
+      //driver.quit();
+	}
+
+
+}
