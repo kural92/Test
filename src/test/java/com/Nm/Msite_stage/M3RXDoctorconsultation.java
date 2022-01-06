a878d98cb3c1de44dfb30a57003a683a96ee3a07
Bala_Santhosh <Balasanthoshkumar.r@netmeds.com>
2022-01-06 18:31:26 +0530

    test

--- src/test/java/com/Nm/Msite_stage/M3RXDoctorconsultation.java
+++ src/test/java/com/Nm/Msite_stage/M3RXDoctorconsultation.java
@@ -289,7 +289,7 @@ public class M3RXDoctorconsultation extends MsiteBaseClass {
 			
 //Modify
 	//	driver.swipe(0, 1500, 0, 1300,1000);
-			driver.swipe(0, 500, 0, 0, 1000);
+			//driver.swipe(0, 500, 0, 0, 1000);
 		Thread.sleep(5000);
 		btnclick(m.getModifyaddressbtn());
 		Thread.sleep(5000);
